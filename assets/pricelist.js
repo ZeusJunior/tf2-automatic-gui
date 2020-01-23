cac679ebb5dfea11fcc64aa7c4c37debf8a5700d
Zeus <zeusjunioryt@gmail.com>
2020-01-23 16:58:47 +0100

    Add manage item form

--- assets/pricelist.js
+++ assets/pricelist.js
@@ -12,4 +12,20 @@ $(document).ready(function() {
 
     $("table").DataTable();
     $('.dataTables_length').addClass('bs-select');
+
+    $('.table > tbody > tr').css('cursor', 'pointer');
+    $('.table > tbody > tr').click(function() {
+        var info = $(this);
+        // Set some values
+        $("#priceModal").find("#pricesku").val(info.data('sku'));
+        $("#priceModal").find("#pricename").val(info.data('name'));
+        $('#priceModal').find("#sellkeys").val(info.data('sell_keys'));
+        $('#priceModal').find("#sellmetal").val(info.data('sell_metal'));
+        $('#priceModal').find("#buykeys").val(info.data('buy_keys'));
+        $('#priceModal').find("#buymetal").val(info.data('buy_metal'));
+        $("#priceintentdiv select").val(info.data('intent'));
+        $('#priceModal').find("#priceminimum").val(info.data('min'));
+        $('#priceModal').find("#pricemaximum").val(info.data('max'));
+        $('#priceModal').modal('show');  
+    });
 });
