5fe51d7e336f706fb259c3a19de1ebc34fc09f67
Max <maliarmaximilian@gmail.com>
2020-03-30 21:32:36 +0200

    started working on trade screen

--- app/trades.js
+++ app/trades.js
@@ -1,14 +1,27 @@
 const fs = require('fs-extra');
 const paths = require('../resources/paths');
 const getName = require('../utils/getName');
-const _ = require('lodash');
-
+const data = require('./data');
+const moment = require('moment');
+const getImage = require('../utils/getImage');
+const SKU = require('tf2-sku');
 
 exports.get = function() {
 	return fs.readJSON(paths.files.polldata)
 		.then((polldata) => {
 			const trades = [];
 
+			Object.keys(polldata.sent).forEach((key)=>{
+				const offer = polldata.offerData[key];
+				if (!checkTradeRecord(offer)) return; // offer must have essential data to be valid
+				trades.push(generateTrade(polldata, key, offer, 'sent'));
+			});
+			Object.keys(polldata.received).forEach((key)=>{
+				const offer = polldata.offerData[key];
+				if (!checkTradeRecord(offer)) return; // offer must have essential data to be valid
+				trades.push(generateTrade(polldata, key, offer, 'received'));
+			});
+			/*
 			_.forOwn(polldata.offerData, (offer, id) => {
 				const accepted = offer.isAccepted ? 'Yes' : 'No';
 				
@@ -22,7 +35,7 @@ exports.get = function() {
 				};
 
 				trades.push(data);
-			});
+			});*/
 
 			return trades;
 		});
@@ -47,26 +60,76 @@ function getDate(unix) {
 }
 
 /**
- * Get items from offer formatted with amount.
- * @param {Object} offer The offerData entry .
- * @param {string} whose Whose items to get from the trade.
- * @return {string[]} Formatted item string names.
+ * Generates time since start of day
+ * @param {number} unix Unix times
+ * @return {number} Seconds since start of day
  */
-function getItemsFromOffer(offer, whose) {
-	const items = [];
+function getTime(unix) {
+	let time = moment.unix(unix);
+	const dayStart = time.startOf('day').unix();
+	time -= dayStart;
+	return time;
+}
 
-	if (offer.dict) {
-		_.forOwn(offer.dict[whose], (amount, sku) => {
-			let itemStr = getName(sku);
-			if (amount > 1) {
-				itemStr += ' x' + amount;
-			}
-	
-			items.unshift(itemStr);
+/**
+ * Checks if offer has all necesary data
+ * @param {Object} offer offer record to check
+ * @return {boolean} valid
+ */
+function checkTradeRecord(offer) {
+	if (typeof offer === 'undefined') return false;
+	return Object.prototype.hasOwnProperty.call(offer, 'partner') && Object.prototype.hasOwnProperty.call(offer, 'dict');
+}
+/**
+ * Generates trade object
+ * @param {Object} polldata raw trade data
+ * @param {String} key offerID 
+ * @param {Object} offer polldata.offerData[key]
+ * @param {String} type offer type - sent/recieved
+ * @return {object} trade object
+ */
+function generateTrade(polldata, key, offer, type) {
+	const trade = {
+		id: key,
+		type: type,
+		lastState: data.ETradeOfferState[polldata.sent[key]],
+		lastChange: polldata.timestamps[key],
+		items: {
+			our: [],
+			their: []
+		},
+		partner: offer.partner,
+		accepted: offer.accepted,
+		date: getDate(offer.finishTimestamp),
+		time: getTime(offer.finishTimestamp),
+		actionTime: offer.actionTime,
+		confirmationTime: offer.confirmationTime,
+		actedOnConfirmation: offer.actedOnConfirmation,
+		value: offer.value,
+		prices: offer.prices
+	};
+	if (Object.keys(offer.dict.our).length > 0) {
+		Object.keys(offer.dict.our).forEach((k)=>{
+			trade.items.our.push(createTradeItem(k, offer.dict.our[k]));
 		});
-
-		return items;
 	}
-
-	return [];
+	if (Object.keys(offer.dict.their).length > 0) {
+		Object.keys(offer.dict.their).forEach((k)=>{
+			trade.items.our.push(createTradeItem(k, offer.dict.our[k]));
+		});
+	}
+	return trade;
+}
+/**
+ * Creates item object
+ * @param {String} sku 
+ * @param {number} amount 
+ * @return {Object} item object created
+ */
+function createTradeItem(sku, amount) {
+	const item = SKU.fromString(sku);
+	item.image = getImage.getImage(item.defindex);
+	item.amount = amount;
+	item.name=getName(sku);
+	return item;
 }
