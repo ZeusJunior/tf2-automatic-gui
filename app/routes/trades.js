2929bdd88d5fdd5500787ce87ed52899d478a4f7
Max <maliarmaximilian@gmail.com>
2020-04-06 22:20:17 +0200

    Added the possibility to get trades as JSON

--- app/routes/trades.js
+++ app/routes/trades.js
@@ -5,24 +5,44 @@ const fs = require('fs-extra');
 const paths = require('../../resources/paths');
 
 router.get('/', (req, res) => {
-	if (!fs.existsSync(paths.files.polldata)) {
-		res.render('trades', {
-			data: null,
-			polldata: false
-		});
-		return;
-	}
-
-	trades.get()
-		.then((data) => {
+	if (req.query.json!='true') {
+		if (!fs.existsSync(paths.files.polldata)) {
 			res.render('trades', {
-				data: data,
-				polldata: true
+				data: null,
+				polldata: false
 			});
-		})
-		.catch((err) => {
-			throw err;
-		});
+			return;
+		}
+	
+		trades.get()
+			.then((data) => {
+				res.render('trades', {
+					data: data,
+					polldata: true
+				});
+			})
+			.catch((err) => {
+				throw err;
+			});
+	} else {
+		if (!fs.existsSync(paths.files.polldata)) {
+			res.json({
+				success: 0
+			});
+			return;
+		}
+	
+		trades.get()
+			.then((data) => {
+				res.json({
+					success: 1,
+					data
+				});
+			})
+			.catch((err) => {
+				throw err;
+			});
+	}
 });
 
 module.exports = router;
