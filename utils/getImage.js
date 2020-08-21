3bf6d9b78e37b0cc5500effd8536a87914016cb1
idinium96 <idin100msc@gmail.com>
2020-08-21 12:16:58 +0800

    get paint can images from SCM

--- utils/getImage.js
+++ utils/getImage.js
@@ -16,7 +16,7 @@ function getImageFromSKU(sku) {
 		return;
 	}
 	if ((Object.keys(paintCanColors).indexOf(sku)) > -1) {
-		found.image_url = found.image_url_large = `https://backpack.tf/images/440/cans/Paint_Can_${paintCanColors[sku]}.png`;
+		found.image_url = found.image_url_large = `https://steamcommunity-a.akamaihd.net/economy/image/IzMF03bi9WpSBq-S-ekoE33L-iLqGFHVaU25ZzQNQcXdEH9myp0erksICf${paintCanColors[sku]}188x188`;
 	} else if (item.paintkit !== null ) {
 		found.image_url = found.image_url_large = `https://scrap.tf/img/items/warpaint/${encodeURIComponent(found.item_name)}_${item.paintkit}_${item.wear}_${item.festive===true?1:0}.png`;
 	} else if (item.australium === true) {
@@ -25,7 +25,7 @@ function getImageFromSKU(sku) {
 	return {
 		small: found.image_url,
 		large: found.image_url_large,
-		effect: item.effect ? `https://backpack.tf/images/440/particles/${item.effect}_188x188.png` : ''
+		effect: item.effect ? `https://marketplace.tf/images/particles/${item.effect}_188x188.png` : ''
 	};
 };
 exports.getImageFromSKU = getImageFromSKU;
